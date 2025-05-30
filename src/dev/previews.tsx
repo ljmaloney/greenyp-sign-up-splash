458c7a7a2eb8522baa9e41f3d1267e4d1597aa9d
ljmaloney <maloney1@mindspring.com>
2025-05-30 14:55:13 -0400

    checkpoint ui

---
+++ src/dev/previews.tsx
@@ -0,0 +1,15 @@
+import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
+import {PaletteTree} from "./palette";
+import Index from "@/pages";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/Index">
+                <Index/>
+            </ComponentPreview>
+        </Previews>
+    );
+};
+
+export default ComponentPreviews;
