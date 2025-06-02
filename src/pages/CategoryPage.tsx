086e6b5a21c77c7d18686881e109a73d7e14b88f
ljmaloney <maloney1@mindspring.com>
2025-06-02 15:42:36 -0400

    change header bar on category page

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import Header from '@/components/Header';
+import Header from '@/components/PublicHeader';
 import Footer from '@/components/Footer';
 import { Button } from "@/components/ui/button";
 import { MapPin, Phone, Star } from 'lucide-react';
