4f7afb7426a9c702fdd1282b0ef0bc487580a139
ljmaloney <maloney1@mindspring.com>
2025-06-23 17:49:49 -0400

    remove crap components

--- src/components/profile/ProfileContent.tsx
+++ src/components/profile/ProfileContent.tsx
@@ -13,14 +13,6 @@ const ProfileContent = ({ profile }: ProfileContentProps) => {
   return (
     <div className="container mx-auto px-4 py-8 max-w-4xl">
       <ProfileHeader profile={profile} />
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-8">
-        <div className="lg:col-span-2">
-          <ProfileDetails profile={profile} />
-        </div>
-        <div>
-          <ProfileHours hours={profile.locationHours} />
-        </div>
-      </div>
     </div>
   );
 };
