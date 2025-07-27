6225fff4c761e075f6020b2ba0412a0f8da88381
ljmaloney <maloney1@mindspring.com>
2025-07-27 15:40:39 -0400

    rename files for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,6 @@ import Classifieds from '@/pages/Classifieds';
 import Samples from '@/pages/classifieds/Samples.tsx';
 import CreateAd from '@/pages/classifieds/CreateAd';
 import UploadImages from '@/pages/classifieds/UploadImages';
-import Payment from '@/pages/classifieds/Payment';
-import PaymentConfirmation from '@/pages/classifieds/PaymentConfirmation';
 import SearchResultsClassifieds from '@/pages/classifieds/SearchResults';
 import ClassifiedDetail from '@/pages/classifieds/ClassifiedDetail';
 import CategoryDescriptions from '@/pages/classifieds/CategoryDescriptions';
@@ -55,6 +53,8 @@ import AdminClassifieds from '@/pages/admin/Classifieds';
 import AdminInvoices from '@/pages/admin/Invoices';
 import AdminPermissions from '@/pages/admin/Permissions';
 import AdminSettings from '@/pages/admin/Settings';
+import ClassifiedPayment from "@/pages/classifieds/ClassifiedPayment.tsx";
+import ClassifiedPaymentConfirmation from "@/pages/classifieds/ClassifiedPaymentConfirmation.tsx";
 
 const queryClient = new QueryClient();
 
@@ -185,8 +185,8 @@ function App() {
               <Route path="/classifieds/samples" element={<Samples />} />
               <Route path="/classifieds/create" element={<CreateAd />} />
               <Route path="/classifieds/uploadimages/:classifiedId" element={<UploadImages />} />
-              <Route path="/classifieds/payment/:classifiedId" element={<Payment />} />
-              <Route path="/classifieds/payment/confirmation/:classifiedId" element={<PaymentConfirmation />} />
+              <Route path="/classifieds/payment/:classifiedId" element={<ClassifiedPayment />} />
+              <Route path="/classifieds/payment/confirmation/:classifiedId" element={<ClassifiedPaymentConfirmation />} />
               <Route path="/classifieds/search" element={<SearchResultsClassifieds />} />
               <Route path="/classifieds/categories" element={<CategoryDescriptions />} />
               <Route path="/classifieds/detail/:id" element={<ClassifiedDetail />} />
