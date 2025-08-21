873ca97cf805161e4c820e365bb7c65e22a96883
ljmaloney <maloney1@mindspring.com>
2025-08-20 20:21:15 -0400

    fix products load for profile page

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,17 +1,15 @@
 
 import { useQuery } from '@tanstack/react-query';
 import { fetchProducts } from '@/services/productService';
-import { useApiClient } from '@/hooks/useApiClient';
 
 export const useProducts = (producerId: string | undefined, locationId: string | undefined) => {
   console.log('useProducts called with:', { producerId, locationId });
-  const apiClient = useApiClient();
   
   return useQuery({
     queryKey: ['products', producerId, locationId],
     queryFn: () => {
       console.log('Executing fetchProducts with:', { producerId, locationId });
-      return fetchProducts(apiClient, producerId!, locationId!);
+      return fetchProducts(producerId, locationId);
     },
     enabled: !!producerId && !!locationId,
   });
