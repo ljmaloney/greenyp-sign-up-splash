c34c03b6fb8ab9e9631af602c00161da5fba5d26
ljmaloney <maloney1@mindspring.com>
2026-04-29 16:12:50 -0400

    update vite version and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -24,5 +24,8 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 5000,
   },
 }));
